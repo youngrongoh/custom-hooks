eb1d8df2a60b36ba66a905224ddd46335706c99a
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-09 14:00:39 +0900

    [Add] useTabs

--- useInput/useInput.js
+++ useInput/useInput.js
@@ -1,16 +1,29 @@
 export const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
+
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
+
     let willUpdate = true;
+
     if (typeof validator === 'function') {
       willUpdate = validator(value);
     }
+
     if (willUpdate) {
       setValue(value);
     }
-    return { value, onChange };
   };
+
+  return { value, onChange };
+};
+
+export const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) return;
+
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+
+  return { currentItem: allTabs[currentIndex], changeItem: setCurrentIndex };
 };
