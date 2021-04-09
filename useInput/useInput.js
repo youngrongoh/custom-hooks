13284120a210d9dc2fba33c6c6bbe6df915fc6b8
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-09 15:22:40 +0900

    [Add] useHover

--- useInput/useInput.js
+++ useInput/useInput.js
@@ -57,3 +57,23 @@ export const useClick = (onClick) => {
   }, []);
   return element;
 };
+
+export const useClick = (onClick) => {
+  if (typeof onClick !== 'function') {
+    return;
+  }
+
+  const element = useRef();
+
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener('mouseenter', onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener('mouseenter', onClick);
+      }
+    };
+  }, []);
+  return element;
+};
