23a3f21e709d14b9e73f73d3a759261eee5eb892
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-09 14:25:46 +0900

    [Add] useClick

--- useInput/useInput.js
+++ useInput/useInput.js
@@ -37,3 +37,23 @@ export const useTitle = (initialTitle) => {
   useEffect(updateTitle, [title]);
   return setTitle;
 };
+
+export const useClick = (onClick) => {
+  if (typeof onClick !== 'function') {
+    return;
+  }
+
+  const element = useRef();
+
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener('click', onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener('click', onClick);
+      }
+    };
+  }, []);
+  return element;
+};
