918b113f2a21aa29d0c1fd91257990635b18fc41
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-10 21:16:50 +0900

    [Add] useBeforeLeave

--- nomad-custom-hooks.js
+++ nomad-custom-hooks.js
@@ -109,3 +109,17 @@ export const usePreventLeave = () => {
 
   return { enablePrevent, disablePrevent };
 };
+
+export const useBeforeLeave = (onBefore) => {
+  if (typeof onBefore !== 'function') return;
+  const handle = (event) => {
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onBefore();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('mouseleave', handle);
+    return () => document.removeEventListener('mouseleave', handle);
+  }, []);
+};
