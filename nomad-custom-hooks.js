24a29c0870146d541de674f54f390cd22a8e9c16
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-11 14:05:10 +0900

    [Add] useNetwork

--- nomad-custom-hooks.js
+++ nomad-custom-hooks.js
@@ -136,3 +136,22 @@ export const useFadeIn = (duration = 1, delay = 0) => {
   }, []);
   return { ref: element, style: { opacity: 0 } };
 };
+
+export const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine);
+  const handleChange = () => {
+    if (typeof onChange === 'function') {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+  useEffect(() => {
+    window.addEventListener('online', handleChange);
+    window.addEventListener('offline', handleChange);
+    return () => {
+      window.removeEventListener('online', handleChange);
+      window.removeEventListener('offline', handleChange);
+    };
+  }, []);
+  return status;
+};
