6a959b25b2fc07b110fc44bfb8e708af8c54d751
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-11 14:04:50 +0900

    [Add] useFadeIn

--- nomad-custom-hooks.js
+++ nomad-custom-hooks.js
@@ -123,3 +123,16 @@ export const useBeforeLeave = (onBefore) => {
     return () => document.removeEventListener('mouseleave', handle);
   }, []);
 };
+
+export const useFadeIn = (duration = 1, delay = 0) => {
+  if (typeof duration !== 'number' || typeof delay !== 'number') return;
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
+  }, []);
+  return { ref: element, style: { opacity: 0 } };
+};
