7828b28835825917ff101e6774ba592dc618df80
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-12 15:23:05 +0900

    [Add] useScroll

--- nomad-custom-hooks.js
+++ nomad-custom-hooks.js
@@ -155,3 +155,25 @@ export const useNetwork = (onChange) => {
   }, []);
   return status;
 };
+
+export const useScroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const onScroll = () => {
+    setState({
+      y: window.scrollY,
+      x: window.scrollX,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  return state;
+};
