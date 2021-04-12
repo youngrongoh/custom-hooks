4a9b54d509bebf71232a595eb17528da05222a5d
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-12 15:24:26 +0900

    [Add] useFullscreen

--- nomad-custom-hooks.js
+++ nomad-custom-hooks.js
@@ -177,3 +177,47 @@ export const useScroll = () => {
 
   return state;
 };
+
+export const useFullscreen = (callback) => {
+  const element = useRef();
+
+  const runCb = (isFull) => {
+    if (callback && typeof callback === 'function') {
+      callback(isFull);
+    }
+  };
+
+  const triggerFullscreen = () => {
+    if (!element.current) return;
+
+    if (element.current.requestFullscreen) {
+      element.current.requestFullscreen();
+    } else if (element.current.mozRequestFullScreen) {
+      element.current.mozRequestFullScreen();
+    } else if (element.current.webkitRequestFullscreen) {
+      element.current.webkitRequestFullscreen();
+    } else if (element.current.msRequestFullscreen) {
+      element.current.msRequestFullscreen();
+    }
+
+    runCb(true);
+  };
+
+  const exitFullscreen = () => {
+    if (!element.current) return;
+
+    if (element.current.exitFullscreen) {
+      element.current.exitFullscreen();
+    } else if (element.current.mozCancelFullScreen) {
+      element.current.mozCancelFullScreen();
+    } else if (element.current.webkitExitFullscreen) {
+      element.current.webkitExitFullscreen();
+    } else if (element.current.msExitFullscreen) {
+      element.current.msExitFullscreen();
+    }
+
+    runCb(false);
+  };
+
+  return { element, triggerFullscreen, exitFullscreen };
+};
