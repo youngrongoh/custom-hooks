1b6e4ab7070d8e48569588f2cfa75551d793ff5f
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-13 20:02:59 +0900

    [Add] useNotification

--- nomad-custom-hooks.js
+++ nomad-custom-hooks.js
@@ -221,3 +221,23 @@ export const useFullscreen = (callback) => {
 
   return { element, triggerFullscreen, exitFullscreen };
 };
+
+export const useNotification = (title, options) => {
+  if (!('Notification' in window)) {
+    return;
+  }
+  const fireNotification = () => {
+    if (Notification.permission !== 'granted') {
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNotification;
+};
